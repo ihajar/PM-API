cae4d4c3de386689a7ecf494328e7dc19a624515
macbook <macbook@MacBook-Air-ihajar.local>
2025-04-07 20:52:45 +0100

    1: first commit

---
+++ patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
@@ -0,0 +1,26 @@
+package com.pm.patient_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PatientRequestDTO {
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name should not exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String Address;
+
+    @NotBlank(message = "Date of Birth is required")
+    private String dateOfBirth;
+
+    @NotBlank(message ="Registered date is required")
+    private String registeredDate;
+}
